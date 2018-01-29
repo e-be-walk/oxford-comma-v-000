517d3f2d49dc11e47dbc40767cca42818b8afde4
Erin Walker <erinb0317@hotmail.com>
2018-01-29 00:31:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-  array.join
   if array.index_length == 1
     array.split("and ")
   elsif array.index_length == 2
     array.split(", ", "and ")
+  else array.join
   end
 
 end
